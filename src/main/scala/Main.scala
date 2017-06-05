0285fbf5d54184d4ce59f0a844ab4960b9328102
Fraser <frasers@hotmail.co.uk>
2017-06-05 15:18:10 +0000

    added a function that loops a defined number of times and then displays two strings depending on if the loop counter divisible by 3 or 5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,26 +2,53 @@
   * Created by Fraser on 05/06/2017.
   */
 object Main {
-  def displayStringXTimes(text : String, timesToLoop : Int) : Unit= {
-    for(i<-1 to timesToLoop)
-    {
+  def displayStringXTimes(text: String, timesToLoop: Int): Unit = {
+    for (i <- 1 to timesToLoop) {
       println(text)
     }
   }
 
-  def displayStringXTimesFormatted(text : String, timesToLoop : Int) : Unit= {
-    for(i<-1 to timesToLoop){
-      for(i<-1 to timesToLoop)
-      {
-       print(f"$text ")
+  def displayStringXTimesFormatted(text: String, timesToLoop: Int): Unit = {
+    for (i <- 1 to timesToLoop) {
+      for (i <- 1 to timesToLoop) {
+        print(f"$text ")
       }
 
       print("\n")
     }
   }
 
-  def main(args : Array[String]) : Unit = {
+  def displayStringsXTimesWithGuards(text1 : String, text2 : String, timesToLoop : Int) : Unit = {
+    for(i <- 1 to timesToLoop){
+      //if i is divisible by 3 and 5 display both strings
+      if(i % 3 == 0 && i % 5 == 0)
+      {
+        print(text1+text2)
+        //if i is divisible by three display first string
+      } else if(i % 3 == 0)
+      {
+
+        print(text1)
+        //else if i is divisible by three display first string
+      } else if(i % 5 == 0)
+      {
+        print(text2)
+        //else display i
+      } else {
+        print(i)
+      }
+
+      //add a comma if not at end of loop
+      if(i < timesToLoop)
+        print(",")
+      else
+        print("\n")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
     displayStringXTimes("Hi", 3)
     displayStringXTimesFormatted("H", 4)
+    displayStringsXTimesWithGuards("Fizz", "Buzz", 15);
   }
 }
